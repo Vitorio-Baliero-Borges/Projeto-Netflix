84e9ba15496d202ac29fa2802beb749d3c8cd602
Vitorio-Baliero-Borges <85197278+Vitorio-Baliero-Borges@users.noreply.github.com>
2021-06-15 22:01:53 -0300

    Add files via upload

---
+++ js/owl/setup.js
@@ -0,0 +1,19 @@
+// iniciando o carrousel
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:false,
+    autoHeight: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+});
